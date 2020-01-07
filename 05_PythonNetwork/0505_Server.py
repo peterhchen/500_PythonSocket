231295b53b96715a60bc0d8d83678999de51257d
peterhchen <peter.hp.chen@gmail.com>
2020-01-06 21:44:59 -0800

    Delete 0505_Server.py

--- 05_PythonNetwork/0505_Server.py
+++
@@ -1,9 +0,0 @@
-# https://www.youtube.com/watch?v=Lbfe3-v7yE0
-import socket
-s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234)) # you can use any 4 digit port which is not occuppied
-s.listen (5)    # 5 is the length of queue.
-while True:
-    clientsocket, address = s.accept()
-    print (f"Connection from {address} has been established")
-    clientsocket.send(bytes ("Welcome to the server!", "utf-8"))
